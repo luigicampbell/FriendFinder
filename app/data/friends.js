5897ccfa2affc39433b58099b0c046953fc6593c
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-03-04 23:39:59 -0800

    created friends array

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+let friendsArray = [
+  {
+    name: "Monica Geller",
+    photo: "http://friends.wikia.com/wiki/Monica_Geller-Bing",
+    scores: [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  },
+  {
+    name: "Ross Geller",
+    photo: "https://www.collegemagazine.com/11-ways-beat-stress-life-feels-like-mess/",
+    scores: [
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      1,
+      2
+    ]
+  },
+  {
+    name: "Chandler Bing",
+    photo: "http://www.vulture.com/2017/03/here-is-matthew-perrys-favorite-chandler-bing-one-liner.html",
+    scores: [
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+  },
+  {
+    name: "Rachel Green",
+    photo: "http://www.comedycentral.co.uk/friends/articles/the-definitive-ranking-of-rachel-green-haircuts",
+    scores: [
+      3,
+      9,
+      1,
+      9,
+      9,
+      2,
+      5,
+      9,
+      4,
+      1
+    ]
+  },
+  {
+    name: "Joey Tribbiani",
+    photo: "https://www.bustle.com/p/11-ways-joey-tribbiani-of-friends-changed-from-the-pilot-to-the-finale-78294",
+    scores: [
+      6,
+      9,
+      6,
+      9,
+      6,
+      9,
+      6,
+      9,
+      6,
+      9
+    ]
+  },
+  {
+    name: "Phoebe Buffay",
+    photo: "https://www.bustle.com/p/11-ways-joey-tribbiani-of-friends-changed-from-the-pilot-to-the-finale-78294",
+    scores: [
+      2,
+      5,
+      6,
+      9,
+      1,
+      4,
+      5,
+      8,
+      2,
+      3
+    ]
+  }
+];
+
+module.exports = friendsArray;
